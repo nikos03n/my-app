18424076677937d575d61d2efa7703a3eeb1f60e
nikolay tamazenko <tamazenko@outlook.com>
2020-08-13 17:18:57 +0300

    Создание страницы заказов

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,17 +1,54 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>Orders</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">Orders</h1>
+
+        <v-list two-line subheader>
+          <v-list-item
+            style="background-color: beige;"
+            avatar
+            v-for="order in orders"
+            :key="order.id"
+          >
+            <v-list-item-action>
+              <v-checkbox color="success" :input-value="order.done" @change="markDone(order)"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>{{order.name}}</v-list-item-title>
+              <v-list-item-subtitle>{{order.phone}}</v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-action>
+              <v-btn :to="'/ad/' + order.adId" class="primary">Open</v-btn>
+            </v-list-item-action>
+          </v-list-item>
+        </v-list>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {}
-    }
-  }
+export default {
+  data() {
+    return {
+      orders: [
+        {
+          id: "fds3",
+          name: "Vladilen",
+          phone: "8-921-121-12-12",
+          adId: "123",
+          done: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    markDone(order) {
+      order.done = true;
+    },
+  },
+};
 </script>
--- src/eee.vue
+++ src/eee.vue
@@ -1,83 +1,49 @@
 <template>
-  <div>
-    <v-container fluid>
-      <v-layout row>
-        <v-flex xs12>
-          <v-carousel>
-            <v-carousel-item v-for="ad in ads" :key="ad.id" :src="ad.imageSrc">
-              <div class="car-link">
-                <v-btn class="error" :to="'/ad/' + ad.id">{{ ad.title }}</v-btn>
-              </div>
-            </v-carousel-item>
-          </v-carousel>
-        </v-flex>
-      </v-layout>
-    </v-container>
-    <v-container grid-list-lg>
-      <v-layout row wrap>
-        <v-flex xs12 sm6 md4 v-for="ad of ads" :key="ad.id">
-          <v-card>
-            <v-card-media :src="ad.imageSrc" height="200px"></v-card-media>
-            <v-card-title primary-title>
-              <div>
-                <h3 class="headline mb-0">{{ad.title}}</h3>
-                <div>{{ad.description}}</div>
-              </div>
-            </v-card-title>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn flat :to="'/ad/' + ad.id">Open</v-btn>
-              <v-btn raised class="primary">Buy</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">Orders</h1>
+
+        <v-list two-line subheader>
+          <v-list-tile avatar v-for="order in orders" :key="order.id">
+            <v-list-tile-action>
+              <v-checkbox color="success" :input-value="order.done" @change="markDone(order)"></v-checkbox>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>{{order.name}}</v-list-tile-title>
+              <v-list-tile-sub-title>{{order.phone}}</v-list-tile-sub-title>
+            </v-list-tile-content>
+
+            <v-list-tile-action>
+              <v-btn :to="'/ad/' + order.adId" class="primary">Open</v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      ads: [
+      orders: [
         {
-          title: "First ad",
-          description: "Hello i am description",
-          promo: false,
-          imageSrc:
-            "https://vuetifyjs.com/static/doc-images/carousel/planet.jpg",
-          id: "123",
-        },
-        {
-          title: "Second ad",
-          description: "Hello i am description",
-          promo: true,
-          imageSrc: "https://vuetifyjs.com/static/doc-images/carousel/bird.jpg",
-          id: "1234",
-        },
-        {
-          title: "Third ad",
-          description: "Hello i am description",
-          promo: true,
-          imageSrc: "https://vuetifyjs.com/static/doc-images/carousel/sky.jpg",
-          id: "12345",
+          id: "fds3",
+          name: "Vladilen",
+          phone: "8-921-121-12-12",
+          adId: "123",
+          done: false,
         },
       ],
     };
   },
+  methods: {
+    markDone(order) {
+      order.done = true;
+    },
+  },
 };
 </script>
-
-<style scoped>
-.car-link {
-  position: absolute;
-  bottom: 50px;
-  left: 50%;
-  background: rgba(0, 0, 0, 0.5);
-  transform: translate(-50%, 0);
-  padding: 5px 15px;
-  border-top-right-radius: 5px;
-  border-top-left-radius: 5px;
-}
-</style>
